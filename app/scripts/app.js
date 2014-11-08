cc79ef7b83163d0a1fc5b63b41939dd2b6ee538a
Isaac Ross <isaac253@gmail.com>
2014-11-08 10:53:26 -0800

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name imageraterApp
+ * @description
+ * # imageraterApp
+ *
+ * Main module of the application.
+ */
+angular.module('imageraterApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.utils',
+    'simpleLogin'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name imageraterApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the imageraterApp
+ */
+angular.module('imageraterApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
